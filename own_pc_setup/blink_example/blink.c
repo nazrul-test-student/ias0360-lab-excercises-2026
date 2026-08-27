3dab20e432648f740a35ac187f1631359c43549f
mohmoh <nazrul.nazeer@taltech.ee>
2026-08-28 00:31:10 +0300

    Added main.c file for blink example

---
+++ own_pc_setup/blink_example/blink.c
@@ -0,0 +1,14 @@
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+int main() {
+    stdio_init_all();
+    cyw43_arch_init();
+
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
